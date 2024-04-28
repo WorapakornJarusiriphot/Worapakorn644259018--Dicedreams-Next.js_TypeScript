e14149bc63be3e7cd97046c3be8a5f140f051668
worapakorn <120018217+WorapakornJarusiriphot@users.noreply.github.com>
2024-04-29 05:55:38 +0700

    git commit -m "Update Sign Up page: refactor form layout, still needs styling adjustments"

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,20 +1,264 @@
-// 'use client';
+'use client';
 
-// import Image from "next/image";
-// import { useState } from "react";
-// // import "./App.css";
-// // import Navbar from './components/Navbar'
-// // import InputFileUpload from './components/FileUpload'
-// // import RegistrationForm from './Page/SignUp'
-// // import EbookFormMui from './Page/Add-book page'
-// import Home from "../Page/Home";
-// import Join from "../Page/Join";
-// import Header from "../components/header/Header";
-// import Footer from "../components/Footer";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import dayjs from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker, DatePickerProps } from '@mui/x-date-pickers/DatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+
+import { styled } from "@mui/material/styles";
+
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+
+import { TextFieldProps } from '@mui/material/TextField';
+
+// function Copyright(props: any) {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+// TODO remove, this demo shouldn't need to reset the theme.
+// กำหนดธีมสีเข้ม
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#fff",
+    },
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "& label": {
+            color: "white",
+          },
+          "& .MuiInputBase-root": {
+            color: "white",
+            "& fieldset": {
+              borderColor: "white",
+            },
+          },
+          "& .MuiOutlinedInput-root": {
+            "&:hover fieldset": {
+              borderColor: "white",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "white",
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+// สร้าง styled component สำหรับ Label แสดงชื่อ Component และประเภทข้อมูล
+const Label = styled(({ className, componentName, valueType }: { className?: string, componentName: string, valueType: string }) => (
+  <span className={className}>
+    <strong>{componentName}</strong> ({valueType})
+  </span>
+))(({ theme }) => ({
+  color: theme.palette.primary.main,
+}));
+
+// ฟังก์ชัน renderInput จะรับ params ของ type TextFieldProps จาก @mui/x-date-pickers
+const renderTextField = (params: TextFieldProps) => (
+  <TextField
+    {...params}
+    InputLabelProps={{ style: { color: 'white' } }}
+    InputProps={{ style: { color: 'white', borderColor: 'white' } }}
+    sx={{
+      '& .MuiOutlinedInput-root': {
+        '&:hover fieldset': { borderColor: 'white' },
+        '&.Mui-focused fieldset': { borderColor: 'white' },
+      },
+    }}
+  />
+);
+
+// กำหนด function สำหรับ renderInput อย่างถูกต้อง
+const renderInput = (params: TextFieldProps) => (
+  <TextField
+    {...params}
+    InputLabelProps={{ style: { color: 'white' } }}
+    InputProps={{ style: { color: 'white', borderColor: 'white' } }}
+    sx={{
+      '& .MuiOutlinedInput-root': {
+        '&:hover fieldset': { borderColor: 'white' },
+        '&.Mui-focused fieldset': { borderColor: 'white' },
+      },
+    }}
+  />
+);
 
 export default function SignUp() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
   return (
-    <>
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            position: 'relative',  // Ensure this is positioned relative to its container
+            zIndex: 2,  // Lower z-index than Header
+          }}
+        >
+          <br />
+          <br />
+          {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar> */}
+          <Typography component="h1" variant="h5">
+            สมัครสมาชิก
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="ชื่อจริง"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="นามสกุล"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <ThemeProvider theme={darkTheme}>
+                  <Grid item xs={12}>
+                    <DatePicker
+                      renderInput={renderTextField}
+                      label="วันเกิด *"
+                    />
+                  </Grid>
+                </ThemeProvider>
+              </LocalizationProvider>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  label="ชื่อผู้ใช้"
+                  name="username"
+                  autoComplete="username"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="phoneNumber"
+                  label="หมายเลขโทรศัพท์"
+                  type="tel"
+                  id="phoneNumber"
+                  autoComplete="tel"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="อีเมล"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="รหัสผ่าน"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              {/* <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid> */}
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 3,
+                mb: 2,
+                color: 'white',  // ตั้งค่าตัวอักษรเป็นสีขาว
+                backgroundColor: 'red',  // ตั้งค่าพื้นหลังเป็นสีแดง
+                '&:hover': {
+                  backgroundColor: 'darkred',  // ตั้งค่าพื้นหลังของปุ่มเมื่อ hover เป็นสีแดงเข้ม
+                }
+              }}
+            >
+              สมัครสมาชิก
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  มีบัญชีอยู่แล้วใช่ไหม? เข้าสู่ระบบ
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        {/* <Copyright sx={{ mt: 5 }} /> */}
+      </Container>
+    </ThemeProvider>
   );
 }
